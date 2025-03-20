841a170beffba789d2c4a7d84997ac5e4e1e234b
TadasA <98015934+t4ov@users.noreply.github.com>
2025-03-20 16:39:54 +0200

    Update osdtest.ps1

--- osdtest.ps1
+++ osdtest.ps1
@@ -1,15 +1,15 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
-# Start-Sleep -Seconds 5
-# #Make sure I have the latest OSD Content
-# Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-# Start-Sleep -Seconds 60
-# Install-Module OSD -Force
-# Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
-# Import-Module OSD -Force
-# #Start OSDCloud ZTI the RIGHT way
-# Write-Host -ForegroundColor Green "Start OSDCloud"
-# Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Professional -ZTI
-# #Restart from WinPE
-# Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
-# Start-Sleep -Seconds 20
-# wpeutil reboot
+Start-Sleep -Seconds 5
+#Make sure I have the latest OSD Content
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Start-Sleep -Seconds 60
+Install-Module OSD -Force
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+#Start OSDCloud ZTI the RIGHT way
+Write-Host -ForegroundColor Green "Start OSDCloud"
+Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Professional -ZTI
+#Restart from WinPE
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
