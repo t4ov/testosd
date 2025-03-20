906df5c2ea9e268f6bef90b8e1d65d6ae795f63d
TadasA <98015934+t4ov@users.noreply.github.com>
2025-03-20 16:00:24 +0200

    Update osdtest.ps1

--- osdtest.ps1
+++ osdtest.ps1
@@ -5,6 +5,7 @@ if ((Get-MyComputerModel) -match ‘Virtual’)
 {Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”Set-DisRes 1600}
 #Make sure I have the latest OSD Content
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Start-Sleep -Seconds 60
 Install-Module OSD -Force
 Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
