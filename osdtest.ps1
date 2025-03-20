1c9ab7077be744a77a988a2df584d36196871d61
TadasA <98015934+t4ov@users.noreply.github.com>
2025-03-20 16:45:05 +0200

    Update osdtest.ps1

--- osdtest.ps1
+++ osdtest.ps1
@@ -2,7 +2,7 @@ Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 #Make sure I have the latest OSD Content
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 5
 Install-Module OSD -Force
 Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
