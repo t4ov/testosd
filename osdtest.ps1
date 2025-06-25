ee1772a29a316bd9e139018efffbe631ad717321
TadasA <98015934+t4ov@users.noreply.github.com>
2025-06-25 13:55:09 +0300

    Update osdtest.ps1

--- osdtest.ps1
+++ osdtest.ps1
@@ -21,6 +21,27 @@ Invoke-RestMethod https://raw.githubusercontent.com/t4ov/testosd/refs/heads/main
 #Invoke-RestMethod http://cleanup.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
 #Invoke-RestMethod http://osdgather.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\osdgather.ps1' -Encoding ascii -Force
 
+$OOBEcmdTasks = @'
+@echo off
+
+REM Wait for Network 10 seconds
+REM ping 127.0.0.1 -n 10 -w 1  >NUL 2>&1
+
+REM Execute OOBE Tasks
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\oobe.ps1
+
+#REM Execute OSD Gather Script
+#REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\osdgather.ps1
+
+#REM Execute Cleanup Script
+#start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\cleanup.ps1
+
+REM Below a PS session for debug and testing in system context, # when not needed 
+REM start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+
+exit 
+'@
+$OOBEcmdTasks | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
 #Restart from WinPE
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
