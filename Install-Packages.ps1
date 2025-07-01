fa436590e940470bb6f06851edc221df6a212b8d
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-01 15:42:56 +0300

    Delete Install-Packages.ps1

--- Install-Packages.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Installing Jumpcloud Agent" -ForegroundColor Green
-start-sleep -s 5
-cd $env:temp | Invoke-Expression; Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1 | Invoke-Expression; ./InstallWindowsAgent.ps1 -JumpCloudConnectKey "jcc_eyJwdWJsaWNLaWNrc3RhcnRVcmwiOiJodHRwczovL2tpY2tzdGFydC5qdW1wY2xvdWQuY29tIiwicHJpdmF0ZUtpY2tzdGFydFVybCI6Imh0dHBzOi8vcHJpdmF0ZS1raWNrc3RhcnQuanVtcGNsb3VkLmNvbSIsImNvbm5lY3RLZXkiOiIyMjE4MDZkZGU0ZjFmYzJjNmI4MWJkNTVjZGQxNjAxMTQ2ZDk3NDNiIn0g"
