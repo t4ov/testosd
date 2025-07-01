d2c38c8c3476b8c96a1020f48b1f497be4d60133
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-01 15:26:09 +0300

    remove unattended install option

--- ScriptPad/Windows11_OOBE.ps1
+++ ScriptPad/Windows11_OOBE.ps1
@@ -78,50 +78,6 @@ exit
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
-$UnattendXml = @'
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-  <settings pass="oobeSystem">
-    <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <InputLocale>en-US</InputLocale>
-      <SystemLocale>en-US</SystemLocale>
-      <UILanguage>en-US</UILanguage>
-      <UserLocale>en-US</UserLocale>
-    </component>
-    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <OOBE>
-        <HideEULAPage>true</HideEULAPage>
-        <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
-        <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
-        <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
-        <NetworkLocation>Work</NetworkLocation>
-        <ProtectYourPC>1</ProtectYourPC>
-      </OOBE>
-      
-      <UserAccounts>
-        <AdministratorPassword>
-          <!-- The Base64-encoded password for the built-in Administrator account. -->
-          <!-- The plaintext password is "ChangeThisP@ssw0rd!". You should change this. -->
-          <Value>QwBoAGEAbgBnAGUAVABoAGkAcwBQAEAAcwBzAHcAMAByAGQAQQAhA==</Value>
-          <PlainText>false</PlainText>
-        </AdministratorPassword>
-      </UserAccounts>
-
-      <RegisteredOrganization>Ovoko</RegisteredOrganization>
-      <RegisteredOwner>Ovoko</RegisteredOwner>
-      <TimeZone>UTC</TimeZone>
-      <ComputerName>*</ComputerName>
-    </component>
-  </settings>
-</unattend>
-'@
-
-$PantherPath = "C:\Windows\Panther"
-If (!(Test-Path $PantherPath)) {
-    New-Item -Path $PantherPath -ItemType Directory -Force | Out-Null
-}
-$UnattendXml | Out-File -FilePath "$PantherPath\Unattend.xml" -Encoding utf8 -Force
-
 #================================================
 #   Restart-Computer
 #================================================
