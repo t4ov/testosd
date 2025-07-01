229dd7252a001c312268094dd6b833dc3e0f79bb
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-01 11:13:01 +0300

    Update Windows11_OOBE.ps1

--- ScriptPad/Windows11_OOBE.ps1
+++ ScriptPad/Windows11_OOBE.ps1
@@ -25,39 +25,6 @@ $Params = @{
     Firmware   = $false
 }
 Start-OSDCloud @Params
-
-
-
-
-#================================================
-#   [PostOS] OOBEDeploy Configuration
-#================================================
-Write-Host -ForegroundColor Green "Creating OOBEDeploy JSON config"
-$OOBEDeployJson = @'
-{
-    "AddNetFX3":  { "IsPresent": true },
-    "Autopilot":  { "IsPresent": false },
-    "RemoveAppx":  [
-        "MicrosoftTeams", "Microsoft.BingWeather", "Microsoft.BingNews", "Microsoft.GamingApp",
-        "Microsoft.GetHelp", "Microsoft.Getstarted", "Microsoft.Messaging", "Microsoft.MicrosoftOfficeHub",
-        "Microsoft.MicrosoftSolitaireCollection", "Microsoft.MicrosoftStickyNotes", "Microsoft.MSPaint",
-        "Microsoft.People", "Microsoft.PowerAutomateDesktop", "Microsoft.StorePurchaseApp", "Microsoft.Todos",
-        "microsoft.windowscommunicationsapps", "Microsoft.WindowsFeedbackHub", "Microsoft.WindowsMaps",
-        "Microsoft.WindowsSoundRecorder", "Microsoft.Xbox.TCUI", "Microsoft.XboxGameOverlay",
-        "Microsoft.XboxGamingOverlay", "Microsoft.XboxIdentityProvider", "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone", "Microsoft.ZuneMusic", "Microsoft.ZuneVideo"
-    ],
-    "UpdateDrivers": { "IsPresent": true },
-    "UpdateWindows": { "IsPresent": true }
-}
-'@
-
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
-
-
 #================================================
 #   [PostOS] OOBE CMD Command Line
 #================================================
@@ -78,49 +45,6 @@ exit
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
 
-$UnattendXml = @'
-<?xml version="1.0" encoding="utf-8"?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-  <settings pass="oobeSystem">
-    <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <InputLocale>en-US</InputLocale>
-      <SystemLocale>en-US</SystemLocale>
-      <UILanguage>en-US</UILanguage>
-      <UserLocale>en-US</UserLocale>
-    </component>
-    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <OOBE>
-        <HideEULAPage>true</HideEULAPage>
-        <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
-        <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
-        <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
-        <NetworkLocation>Work</NetworkLocation>
-        <ProtectYourPC>1</ProtectYourPC>
-      </OOBE>
-      
-      <UserAccounts>
-        <AdministratorPassword>
-          <!-- The Base64-encoded password for the built-in Administrator account. -->
-          <!-- The plaintext password is "ChangeThisP@ssw0rd!". You should change this. -->
-          <Value>QwBoAGEAbgBnAGUAVABoAGkAcwBQAEAAcwBzAHcAMAByAGQAQQAhA==</Value>
-          <PlainText>false</PlainText>
-        </AdministratorPassword>
-      </UserAccounts>
-
-      <RegisteredOrganization>Ovoko</RegisteredOrganization>
-      <RegisteredOwner>Ovoko</RegisteredOwner>
-      <TimeZone>UTC</TimeZone>
-      <ComputerName>*</ComputerName>
-    </component>
-  </settings>
-</unattend>
-'@
-
-$PantherPath = "C:\Windows\Panther"
-If (!(Test-Path $PantherPath)) {
-    New-Item -Path $PantherPath -ItemType Directory -Force | Out-Null
-}
-$UnattendXml | Out-File -FilePath "$PantherPath\Unattend.xml" -Encoding utf8 -Force
 
 #================================================
 #   Restart-Computer
