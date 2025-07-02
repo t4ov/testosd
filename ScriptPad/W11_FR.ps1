ebe41b3e4962af3de3fa04e709f2c1e040dcdfbd
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-02 09:33:02 +0300

    Create W11_FR.ps1

---
+++ ScriptPad/W11_FR.ps1
@@ -0,0 +1,86 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+#================================================
+#   [OS] Params and Start-OSDCloud
+#================================================
+$Params = @{
+    OSVersion  = "Windows 11"
+    OSBuild    = "24H2"
+    OSEdition  = "Pro"
+    OSLanguage = "en-us"
+    OSLicense  = "Retail"
+    ZTI        = $true
+    Firmware   = $true
+}
+Start-OSDCloud @Params
+
+
+
+
+#================================================
+#   [PostOS] OOBEDeploy Configuration
+#================================================
+Write-Host -ForegroundColor Green "Creating OOBEDeploy JSON config"
+$OOBEDeployJson = @'
+{
+    "AddNetFX3":  { "IsPresent": true },
+    "Autopilot":  { "IsPresent": false },
+    "RemoveAppx":  [
+        "MicrosoftTeams", "Microsoft.BingWeather", "Microsoft.BingNews", "Microsoft.GamingApp",
+        "Microsoft.GetHelp", "Microsoft.Getstarted", "Microsoft.Messaging", "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.MicrosoftSolitaireCollection", "Microsoft.MicrosoftStickyNotes", "Microsoft.MSPaint",
+        "Microsoft.People", "Microsoft.PowerAutomateDesktop", "Microsoft.StorePurchaseApp", "Microsoft.Todos",
+        "microsoft.windowscommunicationsapps", "Microsoft.WindowsFeedbackHub", "Microsoft.WindowsMaps",
+        "Microsoft.WindowsSoundRecorder", "Microsoft.Xbox.TCUI", "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay", "Microsoft.XboxIdentityProvider", "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone", "Microsoft.ZuneMusic", "Microsoft.ZuneVideo"
+    ],
+    "UpdateDrivers": { "IsPresent": true },
+    "UpdateWindows": { "IsPresent": true }
+}
+'@
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+
+#================================================
+#   [PostOS] OOBE CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Downloading and creating script for OOBE phase"
+Invoke-RestMethod https://raw.githubusercontent.com/t4ov/testosd/refs/heads/main/Set-GlobalSettings.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\global.ps1' -Encoding ascii -Force
+# Optional scripts (uncomment if needed):
+# Invoke-RestMethod https://raw.githubusercontent.com/t4ov/testosd/refs/heads/main/Install-Packages.ps1 | Out-File -FilePath 'C:\Windows\Setup\scripts\packages.ps1' -Encoding ascii -Force
+# Invoke-RestMethod https://check-autopilotprereq.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\autopilotprereq.ps1' -Encoding ascii -Force
+# Invoke-RestMethod https://start-autopilotoobe.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\autopilotoobe.ps1' -Encoding ascii -Force
+
+$OOBECMD = @'
+@echo off
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\fr.ps1
+:: start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\packages.ps1
+:: start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\autopilotprereq.ps1
+:: start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\autopilotoobe.ps1
+exit
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.cmd' -Encoding ascii -Force
+
+#================================================
+#   Restart-Computer
+#================================================
+Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
+wpeutil reboot
