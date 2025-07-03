e04d9f1ed4d4c4ce60fa70e76887fe2f5790a1d6
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-03 13:29:47 +0300

    Set time zone and language

--- FR.ps1
+++ FR.ps1
@@ -1,4 +1,4 @@
-$Title = "Set-KeyboardLanguage"
+$Title = "Set-KeyboardLanguage-and-TimeZone"
 $host.UI.RawUI.WindowTitle = $Title
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
@@ -8,8 +8,21 @@ $env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
 $Env:PSModulePath = $env:PSModulePath + ";C:\Program Files\WindowsPowerShell\Scripts"
 $env:Path = $env:Path + ";C:\Program Files\WindowsPowerShell\Scripts"
 
-$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage-and-TimeZone.log"
 Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+#================================================
+#  Set Time Zone
+#================================================
+Write-Host -ForegroundColor Green "Setting Time Zone to France (Romance Standard Time)..."
+try {
+    Set-TimeZone -Id "Romance Standard Time" -ErrorAction Stop
+    Write-Host "Time zone set successfully." -ForegroundColor Green
+}
+catch {
+    Write-Warning "Failed to set the time zone. Error: $_"
+}
+#================================================
 
 Write-Host -ForegroundColor Green "Set keyboard language to en-US"
 Start-Sleep -Seconds 5
