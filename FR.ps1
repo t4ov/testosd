1b8d480f64575d050a3b00db701be5f921d5ad63
TadasA <98015934+t4ov@users.noreply.github.com>
2025-07-03 10:06:58 +0300

    remove rename pc logic

--- FR.ps1
+++ FR.ps1
@@ -28,38 +28,6 @@ Start-Sleep -Seconds 5
 #$LanguageList = Get-WinUserLanguageList
 #$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
 #Set-WinUserLanguageList $LanguageList -Force
-<#
-.SYNOPSIS
-    Sets the computer's hostname to "O-FR-<SerialNumber>".
-.DESCRIPTION
-    This script renames the local computer using the BIOS serial number, prefixed with "O-".
-    Requires administrative privileges.
-#>
-
-#Requires -RunAsAdministrator
-
-try {
-    # Retrieve the serial number from BIOS
-    $serialNumber = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber.Trim()
-
-    if ([string]::IsNullOrWhiteSpace($serialNumber)) {
-        throw "Serial number could not be retrieved or is empty."
-    }
-
-    # Format new hostname
-    $newHostname = "O-FR-$serialNumber"
-
-    # Rename the computer
-    Rename-Computer -NewName $newHostname -Force -ErrorAction Stop 
-
-    Write-Host "Hostname successfully changed to $newHostname" -ForegroundColor Green
-    Write-Warning "A system restart is required for the name change to take effect." 
-
-
-} catch {
-    Write-Error "Failed to rename the computer: $_"
-}
-
 
 Start-Sleep -Seconds 10
 Stop-Transcript
